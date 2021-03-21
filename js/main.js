d94f67302bc9af1f62e4a25ee68d3226658796f5
VitalieVSV <dut66AHJCT>
2021-03-21 11:38:53 +0200

    Index html fixed for moved toggle menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const manager = new StyleManager();
-    const toggleMenu = new ToggleMenu();
-    toggleMenu.toggleMenu();
+    manager.toggleMenu();
     manager.liClickHandler();
 });
