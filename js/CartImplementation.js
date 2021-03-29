3d578de21db01ee64557f57380b91ed620e14c52
VitalieVSV <dut66AHJCT>
2021-03-29 18:13:06 +0300

    css fixed shop, fixed header style, fixed style, now header style work for 3 files

--- js/CartImplementation.js
+++ js/CartImplementation.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const manager = new StyleManager();
+    manager.toggleMenu();
     StyleManager.renderCartCount();
     console.log(LocalStorage.cart);
 });
