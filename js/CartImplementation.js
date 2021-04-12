5fbb1bc78d027f3755f97212e4e5e3e474814cf1
VitalieVSV <dut66AHJCT>
2021-04-12 17:15:55 +0300

    removed unnecessary comments

--- js/CartImplementation.js
+++ js/CartImplementation.js
@@ -4,12 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     StyleManager.toggleMenu();
     StyleManager.renderCartCount();
-
-    // if (LocalStorage.cart === "empty") {
-    //     document.querySelector("section .cart__container .cart__empty").classList.remove("disabled");
-    //     document.querySelector("section .cart__container .cart__review").classList.add("disabled");
-    // }
-
     StyleManager.cartStateHandler(LocalStorage.cart === "empty");
 
     checkOut.showCheckOut().then(() => {
